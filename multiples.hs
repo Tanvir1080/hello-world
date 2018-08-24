7342f790b0076fdb1b57c7a3eedcb126afd019e6
Tanvir Islam <islamtanvir8991@gmail.com>
2018-08-24 12:56:24 -0400

    haskell multiples
    adding in more functions

---
+++ multiples.hs
@@ -0,0 +1,9 @@
+{-  Tanvir Islam 
+	Fall -18
+	nid ta940968
+-}
+multipleOf5 n = [x | x <- [1..n], x `mod` 5 == 0]
+
+multipleOf7Or11 n = [x | x <- [1..n], ((x `mod` 7 == 0) || (x `mod` 11 == 0))]
+
+multipleOf3And7 n = [x | x <- [1..n], x `mod` 3 == 0, x `mod` 7 == 0]
